363fe1622428b801b5552b393c289d0273c699d7
Raja Barnwal <rajabarnwal228@gmail.com>
2024-04-05 20:10:17 +0530

    day2

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("h1", {}, "Namaste React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
